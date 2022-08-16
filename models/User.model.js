78838939f7fccbb363bac59378f59c184efb727d
Arinjay Saraf <arinjaysaraf@gmail.com>
2022-08-16 14:30:00 +0530

    notification added

--- models/User.model.js
+++ models/User.model.js
@@ -6,13 +6,17 @@ const UserSchema = new mongoose.Schema(
     name: { type: String, required: true },
     contact: { type: String, required: true },
     gender: { type: String, required: true },
-    pibs: { type: String, required: true },
+    pibs: { type: Array, required: true },
     interests: { type: Array, required: false },
     avatar: { type: String, required: true }, // CDN Img URI
     userType: { type: String, required: true },
     savedArticles: { type: Array, required: false }, // array of articleIds
     rewards: { type: Array, required: false },
     rewardPoints: { type: Number, required: false }, // number of rewardPoints
+    notifications: [{
+      _id : { type: String, required: true },
+      status:{ type: String, required: true }    // read or unread or sent
+    }],
     createdAt: {
       type: String,
       default: new Date().toISOString(),
