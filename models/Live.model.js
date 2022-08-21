86b70f67e37d30066b30bdaabbc8804287b9aa40
Anurag Pal <apal895@gmail.com>
2022-08-21 11:54:52 +0530

    added collection name

--- models/Live.model.js
+++ models/Live.model.js
@@ -1,14 +1,17 @@
 import mongoose from "mongoose";
 
-const LiveVideoSchema = new mongoose.Schema({
+const LiveVideoSchema = new mongoose.Schema(
+  {
     _id: { type: String, required: true },
     title: { type: String, required: true },
     videoUrl: { type: String, required: true },
-    createdAt:{
-        type: String,
-        default: new Date().toISOString(),
-    }
-});
+    createdAt: {
+      type: String,
+      default: new Date().toISOString(),
+    },
+  },
+  { collection: "live" }
+);
 
 const LiveVideoModel = mongoose.model("LiveVideo", LiveVideoSchema);
 
