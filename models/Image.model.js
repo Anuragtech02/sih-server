b05621b96338bd15ddfe0c7615364339bf086afd
Arinjay Saraf <arinjaysaraf@gmail.com>
2022-08-25 08:52:05 +0530

    collections added in models

--- models/Image.model.js
+++ models/Image.model.js
@@ -1,14 +1,17 @@
 import mongoose from "mongoose";
 
-const ImageSchema = new mongoose.Schema({
+const ImageSchema = new mongoose.Schema(
+  {
     _id: { type: String, required: true },
     title: { type: String, required: true },
     src: { type: String, required: true },
-    date:{
-        type: String,
-        default: new Date().toISOString(),
-    }
-});
+    date: {
+      type: String,
+      default: new Date().toISOString(),
+    },
+  },
+  { collection: "users" }
+);
 
 const ImageModel = mongoose.model("Image", ImageSchema);
 
